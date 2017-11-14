e4d4bcc3540c845d9932494eb67aefd1ef1afb0d
eunthebear <eun.david@gmail.com>
2017-11-14 05:58:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+  return drivers.filter(function(element) {
+    return element.toUpperCase() === string.toUpperCase();
+  });
+}
